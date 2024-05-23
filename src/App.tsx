fb17f9cf9fccdd5ccabca2dbbb1837b142303571
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-05-23 17:02:06 -0300

    feat: :tada: first commit

---
+++ src/App.tsx
@@ -0,0 +1,148 @@
+import { useEffect, useState } from "react";
+import logo from "../public/uno-logo.svg";
+import { Button } from "./components/ui/button";
+import { Player } from "./components/ui/player";
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+  DialogClose,
+} from "@/components/ui/dialog";
+import { Input } from "./components/ui/input";
+
+interface Players {
+  playerName: string;
+  pointsAmount: number;
+}
+
+export function App() {
+  const [players, setPlayers] = useState<Players[]>([]);
+
+  const [newPlayerName, setNewPlayerName] = useState("");
+
+  function handleNewGame() {
+    const resetedPlayersPoints = players.map((player) => ({
+      ...player,
+      pointsAmount: 0,
+    }));
+    setPlayers(resetedPlayersPoints);
+  }
+
+  function handleNewPlayer(e: React.FormEvent) {
+    e.preventDefault();
+    if (newPlayerName === "") {
+      return;
+    }
+
+    const newPlayer: Players = {
+      playerName: newPlayerName,
+      pointsAmount: 0,
+    };
+
+    setPlayers([...players, newPlayer]);
+    setNewPlayerName("");
+  }
+
+  function onRemovePlayer(playerName: string) {
+    const filteredPlayers = players.filter(
+      (player) => player.playerName != playerName
+    );
+
+    setPlayers(filteredPlayers);
+  }
+
+  function onAddPoints(playerName: string, points: number) {
+    const updatedPlayerPoints = players.map((player) =>
+      playerName === player.playerName
+        ? { ...player, pointsAmount: player.pointsAmount + points }
+        : player
+    );
+    setPlayers(updatedPlayerPoints);
+  }
+
+  const sortedPlayers = [...players].sort(
+    (a, b) => a.pointsAmount - b.pointsAmount
+  );
+
+  useEffect(() => {
+    const storedPlayers = localStorage.getItem("@uno-contador-players");
+    if (storedPlayers) {
+      const parsedPlayers: Players[] = JSON.parse(storedPlayers);
+      if (parsedPlayers.length > 0) {
+        setPlayers(parsedPlayers);
+      } else {
+        localStorage.removeItem("@uno-contador-players");
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    if (players.length > 0) {
+      localStorage.setItem("@uno-contador-players", JSON.stringify(players));
+    } else {
+      localStorage.removeItem("@uno-contador-players");
+    }
+  }, [players]);
+
+  return (
+    <div className="h-full min-h-screen flex flex-col items-center bg-gray-900 text-gray-100">
+      <header className="w-full flex items-center justify-center gap-2 border-b border-white p-3">
+        <img src={logo} alt="Logo original do UNO" width={150} />
+        <h1 className="text-2xl text-gray-300 uppercase">Contador de pontos</h1>
+      </header>
+
+      <main className="flex flex-col gap-3 flex-1 w-[90%] mx-auto h-auto my-8 max-w-[50rem]">
+        <div className="flex gap-3 justify-between">
+          <Button
+            onClick={handleNewGame}
+            disabled={players.length === 0}
+            className="w-full bg-green-500 text-gray-900 text-lg font-medium hover:bg-green-600"
+          >
+            Novo jogo
+          </Button>
+
+          <Dialog>
+            <DialogTrigger className="w-full">
+              <Button className="w-full bg-green-500 text-gray-900 text-lg font-medium hover:bg-green-600">
+                Novo jogador +
+              </Button>
+            </DialogTrigger>
+            <DialogContent className=" h-52 w-[25rem] rounded-xl">
+              <form
+                onSubmit={handleNewPlayer}
+                className="flex flex-col items-center justify-center gap-3 w-full"
+              >
+                <Input
+                  type="text"
+                  placeholder="Nome do novo jogador"
+                  value={newPlayerName}
+                  onChange={(e) => setNewPlayerName(e.target.value)}
+                />
+                <DialogClose className="w-full">
+                  <Button className="w-full" type="submit">
+                    +
+                  </Button>
+                </DialogClose>
+              </form>
+            </DialogContent>
+          </Dialog>
+        </div>
+
+        <div className="flex flex-col gap-2 my-3">
+          {sortedPlayers.map((player, index) => (
+            <Player
+              key={index}
+              rank={index}
+              playerName={player.playerName}
+              pointsAmount={player.pointsAmount}
+              handleRemovePlayer={() => onRemovePlayer(player.playerName)}
+              handleAddPoints={(points) =>
+                onAddPoints(player.playerName, points)
+              }
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+}
---
+++ src/components/ui/player.tsx
@@ -0,0 +1,104 @@
+import { useState } from "react";
+import { Button } from "./button";
+import { Card, CardContent, CardHeader, CardTitle } from "./card";
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+  DialogClose,
+} from "@/components/ui/dialog";
+import { Input } from "./input";
+
+interface PlayerProps {
+  rank: number;
+  playerName: string;
+  pointsAmount: number;
+  handleRemovePlayer: () => void;
+  handleAddPoints: (points: number) => void;
+}
+
+export function Player({
+  rank,
+  playerName,
+  pointsAmount,
+  handleRemovePlayer,
+  handleAddPoints,
+}: PlayerProps) {
+  const pointsToLoseGame = 500 - pointsAmount;
+  const [newPoints, setNewPoints] = useState<number>(0);
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    handleAddPoints(newPoints);
+    setNewPoints(0); // Reset the input field
+  }
+
+  return (
+    <div
+      className={`flex items-center gap-3 h-32 ${
+        pointsAmount > 500 && "opacity-60"
+      }`}
+    >
+      <Dialog>
+        <DialogTrigger className="w-full">
+          <Card className="overflow-hidden">
+            <CardHeader
+              className={`p-2 flex-row items-center justify-between rounded-lg bg-gray-300 ${
+                pointsAmount > 500 && "bg-red-400"
+              }`}
+            >
+              <CardTitle className="flex gap-3 text-xl items-center">
+                <div className="bg-gray-200 p-2 rounded-full">
+                  {pointsAmount > 500 ? "Jogador eliminado" : `${rank + 1}º`}
+                </div>
+                <span>{playerName}</span>
+              </CardTitle>
+            </CardHeader>
+
+            <CardContent className="p-2 flex justify-center gap-4">
+              <div className="flex flex-col items-center">
+                <p className="font-medium">Pontos acumulados</p>
+                <span className="font-bold ">{pointsAmount}</span>
+              </div>
+
+              <div className="w-1 border-2 rounded-full" />
+
+              <div className="flex flex-col items-center">
+                <p className="font-medium">Pontos para sair</p>
+                <span className="font-bold text-red-500">
+                  {pointsToLoseGame}
+                </span>
+              </div>
+            </CardContent>
+          </Card>
+        </DialogTrigger>
+
+        <DialogContent>
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col items-center justify-center gap-3 w-full"
+          >
+            <Input
+              type="number"
+              value={newPoints}
+              onChange={(e) => setNewPoints(Number(e.target.value))}
+              placeholder="Adicionar pontos"
+            />
+            <DialogClose className="w-full">
+              <Button className="w-full" type="submit">
+                Adicionar
+              </Button>
+            </DialogClose>
+          </form>
+        </DialogContent>
+      </Dialog>
+
+      <Button
+        onClick={handleRemovePlayer}
+        className="text-red-900 font-medium text-xl bg-red-500 h-full hover:bg-red-600 hover:no-underline"
+      >
+        X
+      </Button>
+    </div>
+  );
+}
