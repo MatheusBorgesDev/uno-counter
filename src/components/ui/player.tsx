cbc6f15d7350654519ca69f77584bfed26bcaef3
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-09-13 01:15:13 -0300

    refactor: :recyle: Improve player component logic and UI
    - Refactor the player component to improve logic and UI.
    - Add conditional logic to handle adding points only if the input value is greater than 0.
    - Display round number and checkmark icon when points are scored.
    - Add confirmation dialog for player deletion.

--- src/components/ui/player.tsx
+++ src/components/ui/player.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-
 import { Button } from "./button";
 import { Card, CardContent, CardHeader, CardTitle } from "./card";
 import {
@@ -10,6 +9,7 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "./input";
 import { FaPlus } from "react-icons/fa";
+import { Check, X } from "lucide-react";
 
 interface PlayerProps {
   rank: number;
@@ -17,6 +17,8 @@ interface PlayerProps {
   pointsAmount: number;
   handleRemovePlayer: () => void;
   handleAddPoints: (points: number) => void;
+  isPointsScored: boolean;
+  roundNumber: number;
 }
 
 export function Player({
@@ -25,37 +27,60 @@ export function Player({
   pointsAmount,
   handleRemovePlayer,
   handleAddPoints,
+  isPointsScored,
+  roundNumber,
 }: PlayerProps) {
   const pointsToLoseGame = 500 - pointsAmount;
   const [newPoints, setNewPoints] = useState(0);
 
   function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
-    handleAddPoints(newPoints);
-    setNewPoints(0);
+    if (newPoints > 0) {
+      handleAddPoints(newPoints);
+      setNewPoints(0);
+    }
   }
 
   const eliminatedPlayer = pointsAmount >= 500;
 
   return (
     <div
-      className={`flex items-center gap-3 h-32 ${
-        eliminatedPlayer && "opacity-60"
+      className={`flex items-center gap-3   ${
+        eliminatedPlayer ? "opacity-60" : ""
       }`}
     >
       <Dialog>
-        <DialogTrigger className="w-full truncate">
-          <Card className="overflow-hidden">
+        <DialogTrigger className="w-full truncate" disabled={eliminatedPlayer}>
+          <Card className="overflow-hidden ">
             <CardHeader
               className={`p-2 flex-row items-center justify-between rounded-lg bg-gray-300 ${
                 eliminatedPlayer && "bg-red-400"
               }`}
             >
-              <CardTitle className="flex gap-3 text-xl items-center">
-                <div className="bg-gray-200 p-2 rounded-full">
-                  {eliminatedPlayer ? "Eliminado" : `${rank + 1}º`}
+              <CardTitle className="flex text-lg items-center justify-between w-full">
+                <div className="flex gap-2 items-center">
+                  <div className="bg-gray-200 p-2 rounded-full flex gap-2">
+                    {eliminatedPlayer ? <X size={15} /> : `${rank + 1}º`}
+                  </div>
+                  <span className="">{playerName}</span>
                 </div>
-                <span className="">{playerName}</span>
+
+                <div className="text-sm flex items-center justify-center">
+                  {eliminatedPlayer && (
+                    <span className="text-black">Eliminado</span>
+                  )}
+
+                  {!eliminatedPlayer && !isPointsScored && (
+                    <span>Rodada {roundNumber} ...</span>
+                  )}
+
+                  {!eliminatedPlayer && isPointsScored && (
+                    <div className="flex gap-1 items-center">
+                      Rodada {roundNumber}{" "}
+                      <Check size={18} className="text-green-800" />{" "}
+                    </div>
+                  )}
+                </div>
               </CardTitle>
             </CardHeader>
 
@@ -67,14 +92,18 @@ export function Player({
                 </span>
               </div>
 
-              <div className="w-1 border-2 rounded-full" />
+              {!eliminatedPlayer && (
+                <>
+                  <div className=" border rounded-full" />
 
-              <div className="flex flex-col items-center">
-                <p className="font-medium">Pontos para sair</p>
-                <span className="font-bold text-red-500">
-                  {pointsToLoseGame}
-                </span>
-              </div>
+                  <div className="flex flex-col items-center">
+                    <p className="font-medium">Pontos para sair</p>
+                    <span className="font-bold text-red-500">
+                      {pointsToLoseGame}
+                    </span>
+                  </div>
+                </>
+              )}
             </CardContent>
           </Card>
         </DialogTrigger>
@@ -89,11 +118,14 @@ export function Player({
               value={newPoints}
               onChange={(e) => setNewPoints(Number(e.target.value))}
               placeholder="Adicionar pontos"
+              min="0"
+              className="w-full"
             />
             <DialogClose className="w-full">
               <Button
                 className="w-full flex gap-2 bg-green-500 text-gray-900 text-lg font-medium hover:bg-green-600"
                 type="submit"
+                disabled={eliminatedPlayer}
               >
                 <FaPlus />
               </Button>
@@ -102,12 +134,36 @@ export function Player({
         </DialogContent>
       </Dialog>
 
-      <Button
-        onClick={handleRemovePlayer}
-        className="text-red-900 text-xl font-bold bg-red-500 h-full hover:bg-red-600 hover:no-underline"
-      >
-        X
-      </Button>
+      <Dialog>
+        {!eliminatedPlayer && (
+          <DialogTrigger asChild>
+            <Button className="text-red-900 text-xl font-bold bg-red-500 h-32 hover:bg-red-600 hover:no-underline">
+              X
+            </Button>
+          </DialogTrigger>
+        )}
+
+        <DialogContent className="flex flex-col gap-3 justify-center w-64 h-52 rounded-lg">
+          <p className="text-lg font-medium">
+            Tem certeza que deseja excluir o jogador?
+          </p>
+          <div className="flex gap-3">
+            <DialogClose>
+              <Button className="bg-gray-500 text-gray-100 font-medium">
+                Cancelar
+              </Button>
+            </DialogClose>
+            <DialogClose>
+              <Button
+                className="bg-red-500 text-gray-100 font-medium"
+                onClick={handleRemovePlayer}
+              >
+                Excluir
+              </Button>
+            </DialogClose>
+          </div>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
